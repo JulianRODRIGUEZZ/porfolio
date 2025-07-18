6606e1c84609d5ee14b82c3e0eaa95162fbe58bc
JulianRODRIGUEZZ <julian.9911@hotmail.com>
2025-07-18 11:00:13 -0500

    iconos slider

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,18 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				slide: {
+					'0%': { transform: 'translateX(0%)' },
+					'100%': { transform: 'translateX(-50%)' }, // mueve hacia la izquierda para que los logos parezcan deslizarse a la derecha
+				},
+			},
+			animation: {
+				slide: 'slide 20s linear infinite', // Puedes cambiar 30s por la velocidad que prefieras
+			},
+		},
 	},
 	plugins: [],
 }
+
